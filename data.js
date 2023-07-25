b8146555b8e48cf7eda89ddab797815896559e1d
kadalidileepkumar <112861775+kadalidileepkumar@users.noreply.github.com>
2023-07-25 12:59:54 -0400

    noting

---
+++ data.js
@@ -0,0 +1,579 @@
+const studentData = [
+  {
+    "rollNumber": "20U41A0101",
+    "subjects": [
+      {
+        "subcode": "R2031011",
+        "subname": "STRUCTURAL ANALYSIS",
+        "internals": 18,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031012",
+        "subname": "DESIGN AND DRAWING OF REINFORCED CONCRET",
+        "internals": 24,
+        "grade": "C",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031013",
+        "subname": "GEOTECHNICAL ENGINEERING-1",
+        "internals": 16,
+        "grade": "E",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031014",
+        "subname": "LAB SURVEY CAMP (FIELD WORK)",
+        "internals": 13,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031015",
+        "subname": "GEOTECHNICAL ENGINEERING LAB",
+        "internals": 12,
+        "grade": "A",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031016",
+        "subname": "SKILL ADVANCED COURSE/ SOFT SKILL COURSE",
+        "internals": 0,
+        "grade": "A",
+        "credits": 2
+      },
+      {
+        "subcode": "R2031017",
+        "subname": "(AICTE SUGGESTED) PROFESSIONAL ETHICS AN",
+        "internals": 0,
+        "grade": "COMPLE",
+        "credits": 0
+      },
+      {
+        "subcode": "R2031018",
+        "subname": "SUMMER INTERNSHIP 2MONTHS (MANDATORY) AF",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031019",
+        "subname": "COMMUNITY SERVICE PROJECT",
+        "internals": 0,
+        "grade": "A",
+        "credits": 4
+      },
+      {
+        "subcode": "R203101A",
+        "subname": "CONSTRUCTION TECHNOLOGY &MANAGEMENT",
+        "internals": 18,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R203102F",
+        "subname": "RENEWABLE ENERGY SOURCES",
+        "internals": 18,
+        "grade": "D",
+        "credits": 3
+      }
+    ]
+  },
+  {
+    "rollNumber": "20U41A0102",
+    "subjects": [
+      {
+        "subcode": "R2031011",
+        "subname": "STRUCTURAL ANALYSIS",
+        "internals": 18,
+        "grade": "E",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031012",
+        "subname": "DESIGN AND DRAWING OF REINFORCED CONCRET",
+        "internals": 26,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031013",
+        "subname": "GEOTECHNICAL ENGINEERING-1",
+        "internals": 18,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031014",
+        "subname": "LAB SURVEY CAMP (FIELD WORK)",
+        "internals": 13,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031015",
+        "subname": "GEOTECHNICAL ENGINEERING LAB",
+        "internals": 12,
+        "grade": "A",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031016",
+        "subname": "SKILL ADVANCED COURSE/ SOFT SKILL COURSE",
+        "internals": 0,
+        "grade": "A",
+        "credits": 2
+      },
+      {
+        "subcode": "R2031017",
+        "subname": "(AICTE SUGGESTED) PROFESSIONAL ETHICS AN",
+        "internals": 0,
+        "grade": "COMPLE",
+        "credits": 0
+      },
+      {
+        "subcode": "R2031018",
+        "subname": "SUMMER INTERNSHIP 2MONTHS (MANDATORY) AF",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031019",
+        "subname": "COMMUNITY SERVICE PROJECT",
+        "internals": 0,
+        "grade": "A",
+        "credits": 4
+      },
+      {
+        "subcode": "R203101A",
+        "subname": "CONSTRUCTION TECHNOLOGY &MANAGEMENT",
+        "internals": 22,
+        "grade": "C",
+        "credits": 3
+      },
+      {
+        "subcode": "R203102F",
+        "subname": "RENEWABLE ENERGY SOURCES",
+        "internals": 24,
+        "grade": "C",
+        "credits": 3
+      }
+    ]
+  },
+  {
+    "rollNumber": "20U41A0103",
+    "subjects": [
+      {
+        "subcode": "R2031011",
+        "subname": "STRUCTURAL ANALYSIS",
+        "internals": 19,
+        "grade": "E",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031012",
+        "subname": "DESIGN AND DRAWING OF REINFORCED CONCRET",
+        "internals": 27,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031013",
+        "subname": "GEOTECHNICAL ENGINEERING-1",
+        "internals": 19,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031014",
+        "subname": "LAB SURVEY CAMP (FIELD WORK)",
+        "internals": 14,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031015",
+        "subname": "GEOTECHNICAL ENGINEERING LAB",
+        "internals": 11,
+        "grade": "A",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031016",
+        "subname": "SKILL ADVANCED COURSE/ SOFT SKILL COURSE",
+        "internals": 0,
+        "grade": "A",
+        "credits": 2
+      },
+      {
+        "subcode": "R2031017",
+        "subname": "(AICTE SUGGESTED) PROFESSIONAL ETHICS AN",
+        "internals": 0,
+        "grade": "COMPLE",
+        "credits": 0
+      },
+      {
+        "subcode": "R2031018",
+        "subname": "SUMMER INTERNSHIP 2MONTHS (MANDATORY) AF",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031019",
+        "subname": "COMMUNITY SERVICE PROJECT",
+        "internals": 0,
+        "grade": "A",
+        "credits": 4
+      },
+      {
+        "subcode": "R203101A",
+        "subname": "CONSTRUCTION TECHNOLOGY &MANAGEMENT",
+        "internals": 21,
+        "grade": "C",
+        "credits": 3
+      },
+      {
+        "subcode": "R203102F",
+        "subname": "RENEWABLE ENERGY SOURCES",
+        "internals": 24,
+        "grade": "D",
+        "credits": 3
+      }
+    ]
+  },
+  {
+    "rollNumber": "20U41A0104",
+    "subjects": [
+      {
+        "subcode": "R2031011",
+        "subname": "STRUCTURAL ANALYSIS",
+        "internals": 16,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031012",
+        "subname": "DESIGN AND DRAWING OF REINFORCED CONCRET",
+        "internals": 27,
+        "grade": "B",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031013",
+        "subname": "GEOTECHNICAL ENGINEERING-1",
+        "internals": 18,
+        "grade": "C",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031014",
+        "subname": "LAB SURVEY CAMP (FIELD WORK)",
+        "internals": 15,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031015",
+        "subname": "GEOTECHNICAL ENGINEERING LAB",
+        "internals": 13,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031016",
+        "subname": "SKILL ADVANCED COURSE/ SOFT SKILL COURSE",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 2
+      },
+      {
+        "subcode": "R2031017",
+        "subname": "(AICTE SUGGESTED) PROFESSIONAL ETHICS AN",
+        "internals": 0,
+        "grade": "COMPLE",
+        "credits": 0
+      },
+      {
+        "subcode": "R2031018",
+        "subname": "SUMMER INTERNSHIP 2MONTHS (MANDATORY) AF",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031019",
+        "subname": "COMMUNITY SERVICE PROJECT",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 4
+      },
+      {
+        "subcode": "R203101A",
+        "subname": "CONSTRUCTION TECHNOLOGY &MANAGEMENT",
+        "internals": 21,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R203102F",
+        "subname": "RENEWABLE ENERGY SOURCES",
+        "internals": 17,
+        "grade": "E",
+        "credits": 3
+      }
+    ]
+  },
+  {
+    "rollNumber": "20U41A0105",
+    "subjects": [
+      {
+        "subcode": "R2031011",
+        "subname": "STRUCTURAL ANALYSIS",
+        "internals": 20,
+        "grade": "B",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031012",
+        "subname": "DESIGN AND DRAWING OF REINFORCED CONCRET",
+        "internals": 29,
+        "grade": "B",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031013",
+        "subname": "GEOTECHNICAL ENGINEERING-1",
+        "internals": 21,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031014",
+        "subname": "LAB SURVEY CAMP (FIELD WORK)",
+        "internals": 14,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031015",
+        "subname": "GEOTECHNICAL ENGINEERING LAB",
+        "internals": 14,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031016",
+        "subname": "SKILL ADVANCED COURSE/ SOFT SKILL COURSE",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 2
+      },
+      {
+        "subcode": "R2031017",
+        "subname": "(AICTE SUGGESTED) PROFESSIONAL ETHICS AN",
+        "internals": 0,
+        "grade": "COMPLE",
+        "credits": 0
+      },
+      {
+        "subcode": "R2031018",
+        "subname": "SUMMER INTERNSHIP 2MONTHS (MANDATORY) AF",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031019",
+        "subname": "COMMUNITY SERVICE PROJECT",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 4
+      },
+      {
+        "subcode": "R203101A",
+        "subname": "CONSTRUCTION TECHNOLOGY &MANAGEMENT",
+        "internals": 25,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R203102F",
+        "subname": "RENEWABLE ENERGY SOURCES",
+        "internals": 23,
+        "grade": "D",
+        "credits": 3
+      }
+    ]
+  },
+  {
+    "rollNumber": "20U41A0106",
+    "subjects": [
+      {
+        "subcode": "R2031011",
+        "subname": "STRUCTURAL ANALYSIS",
+        "internals": 17,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031012",
+        "subname": "DESIGN AND DRAWING OF REINFORCED CONCRET",
+        "internals": 25,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031013",
+        "subname": "GEOTECHNICAL ENGINEERING-1",
+        "internals": 19,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031014",
+        "subname": "LAB SURVEY CAMP (FIELD WORK)",
+        "internals": 13,
+        "grade": "A",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031015",
+        "subname": "GEOTECHNICAL ENGINEERING LAB",
+        "internals": 13,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031016",
+        "subname": "SKILL ADVANCED COURSE/ SOFT SKILL COURSE",
+        "internals": 0,
+        "grade": "A",
+        "credits": 2
+      },
+      {
+        "subcode": "R2031017",
+        "subname": "(AICTE SUGGESTED) PROFESSIONAL ETHICS AN",
+        "internals": 0,
+        "grade": "COMPLE",
+        "credits": 0
+      },
+      {
+        "subcode": "R2031018",
+        "subname": "SUMMER INTERNSHIP 2MONTHS (MANDATORY) AF",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031019",
+        "subname": "COMMUNITY SERVICE PROJECT",
+        "internals": 0,
+        "grade": "A",
+        "credits": 4
+      },
+      {
+        "subcode": "R203101A",
+        "subname": "CONSTRUCTION TECHNOLOGY &MANAGEMENT",
+        "internals": 21,
+        "grade": "C",
+        "credits": 3
+      },
+      {
+        "subcode": "R203102F",
+        "subname": "RENEWABLE ENERGY SOURCES",
+        "internals": 21,
+        "grade": "D",
+        "credits": 3
+      }
+    ]
+  },
+  {
+    "rollNumber": "20U41A0526",
+    "subjects": [
+      {
+        "subcode": "R2031019",
+        "subname": "COMMUNITY SERVICE PROJECT",
+        "internals": 0,
+        "grade": "A",
+        "credits": 4
+      },
+      {
+        "subcode": "R203104K",
+        "subname": "IOT AND APPLICATIONS",
+        "internals": 24,
+        "grade": "C",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031051",
+        "subname": "COMPUTER NETWORKS",
+        "internals": 23,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031052",
+        "subname": "DESIGN AND ANALYSIS OF ALGORITHMS",
+        "internals": 27,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031053",
+        "subname": "DATA WAREHOUSING AND DATA MINING",
+        "internals": 22,
+        "grade": "D",
+        "credits": 3
+      },
+      {
+        "subcode": "R2031054",
+        "subname": "DATA WAREHOUSING AND DATA MINING LAB",
+        "internals": 15,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031055",
+        "subname": "COMPUTER NETWORKS LAB",
+        "internals": 14,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R2031057",
+        "subname": "CONTINUOUS INTEGRATION AND CONTINUOUS DE",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 2
+      },
+      {
+        "subcode": "R2031058",
+        "subname": "EMPLOYABILITY SKILLS-I",
+        "internals": 0,
+        "grade": "COMPLE",
+        "credits": 0
+      },
+      {
+        "subcode": "R2031059",
+        "subname": "SUMMER INTERNSHIP 2 MONTHS (MANDATORY) A",
+        "internals": 0,
+        "grade": "A+",
+        "credits": 1.5
+      },
+      {
+        "subcode": "R203105B",
+        "subname": "SOFTWARE PROJECT MANAGEMENT",
+        "internals": 25,
+        "grade": "B",
+        "credits": 3
+      }
+    ]
+  }
+];
+
+
+export default studentData;
